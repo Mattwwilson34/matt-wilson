168ce8cde5e14890a5ae1d307192bfee1186a5c6
Matt Wilson <mattwwilson34@gmail.com>
2022-03-12 16:45:48 -0500

    Integrated React Router + / route

--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -1,8 +1,9 @@
-import './App.css'
+import './App.css';
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const App = () => {
-  return <div>App COMPONENT</div>;
-  }; 
+  return <div></div>;
+};
 
-  export default App;
+export default App;
