14dc3dee65192ff78308b4134654d0d873982592
Matt Wilson <mattwwilson34@gmail.com>
2022-03-12 19:36:06 -0500

    Fixed Footer styling

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -5,11 +5,16 @@ import { Link } from 'react-router-dom';
 const Footer = () => {
   return (
     <div className='Footer'>
-      <Link to='https://www.facebook.com/MattWilsonDPT/'>Facebook</Link>
-      <Link to='https://twitter.com/matt_willsonDPT'>Twitter</Link>
-      <Link to='https://www.linkedin.com/in/mattwilsondpt/'>LinkedIn</Link>
-      <Link to='https://github.com/Mattwwilson34'>GitHub</Link>
-      <Link to='mailto:mattwwilson34@gmail.com'>Email Me</Link>
+      <div className='Foot-Link-Container'>
+        <Link to='https://www.facebook.com/MattWilsonDPT/'>Facebook</Link>
+        <Link to='https://twitter.com/matt_willsonDPT'>Twitter</Link>
+        <Link to='https://www.linkedin.com/in/mattwilsondpt/'>LinkedIn</Link>
+        <Link to='https://github.com/Mattwwilson34'>GitHub</Link>
+        <Link to='mailto:mattwwilson34@gmail.com'>Email Me</Link>
+      </div>
+      <span className='Copyright'>
+        &copy; designed and engineered by Matthew Wilson
+      </span>
     </div>
   );
 };
--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -8,19 +8,19 @@ const Nav = () => {
     <nav className='Nav'>
       <span className='Name'>Matt Wilson</span>
       <div className='Social-Links-Container'>
-        <NavLink to='#'>
+        <NavLink to='https://www.facebook.com/MattWilsonDPT/'>
           <SocialIcon url='https://www.facebook.com/MattWilsonDPT/' />
         </NavLink>
-        <NavLink to='#'>
+        <NavLink to='https://twitter.com/matt_willsonDPT'>
           <SocialIcon url='https://twitter.com/matt_willsonDPT' />
         </NavLink>
-        <NavLink to='#'>
+        <NavLink to='https://www.linkedin.com/in/mattwilsondpt/'>
           <SocialIcon url='https://www.linkedin.com/in/mattwilsondpt/' />
         </NavLink>
-        <NavLink to='#'>
+        <NavLink to='https://github.com/Mattwwilson34'>
           <SocialIcon url='https://github.com/Mattwwilson34' />
         </NavLink>
-        <NavLink to='#'>
+        <NavLink to='mailto:mattwwilson34@gmail.com'>
           <SocialIcon url='mailto:mattwwilson34@gmail.com' />
         </NavLink>
       </div>
